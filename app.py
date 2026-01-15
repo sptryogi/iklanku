f91ce11944b2e1a6a7047f8ac68ec31bbf72b926
sptryogi <115679084+sptryogi@users.noreply.github.com>
2026-01-15 20:21:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -308,12 +308,12 @@ def process_data(platform, toko, inputs):
                 col_widths[col_idx] = width
                 
         # 1. LOAD DATA
-        df_order = pd.read_excel(file_order, dtype={'Total Harga Produk': str, 'Jumlah': str, 'Harga Satuan': str})
-        df_iklan = pd.read_csv(file_iklan, skiprows=7)
+        df_order = pd.read_excel(f_order, dtype={'Total Harga Produk': str, 'Jumlah': str, 'Harga Satuan': str})
+        df_iklan = pd.read_csv(f_iklan, skiprows=7)
         
         # Cek apakah file_seller ada isinya (Optional)
-        if file_seller is not None:
-            df_seller = pd.read_csv(file_seller, dtype={'Pengeluaran(Rp)': str})
+        if f_seller is not None:
+            df_seller = pd.read_csv(f_seller, dtype={'Pengeluaran(Rp)': str})
         else:
             # Jika tidak ada, buat DataFrame kosong dengan kolom minimal agar tidak error saat merge
             df_seller = pd.DataFrame(columns=['Kode Pesanan', 'Pengeluaran(Rp)'])
